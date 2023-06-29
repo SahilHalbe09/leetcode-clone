8f49b6fb1ad5311fc76b31ab1f97f32d957405c2
Sahil Halbe <87558504+SahilHalbe09@users.noreply.github.com>
2023-06-29 16:18:51 +0530

    Update Topbar.tsx

--- components/Topbar/Topbar.tsx
+++ components/Topbar/Topbar.tsx
@@ -72,7 +72,7 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 
 					<div className="flex items-center space-x-4 flex-1 justify-end">
 						<div>
-							<a href="/" target="_blank" rel="noreferrer" className="bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2">
+							<a href="https://www.thesahildev.in/" target="_blank" rel="noreferrer" className="bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2">
 								My Site
 							</a>
 						</div>
